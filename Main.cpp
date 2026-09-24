02e05a8f96e5bc2863676948d0bb426b21a93065
curciog <curciogdev@gmail.com>
2026-09-24 16:25:27 -0300

    implementa cadastro e insercao de computadores

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,43 @@
-#include "Computador.h"
+#include "Computador.h"`
+#include <iostream>
+
+using namespace std;
+
+const int CAPACIDADE = 100;
+
+Computador cadastrarComputador() {
+    Computador computador;
+
+    computador.setDados();
+
+    return computador;
+}
+
+bool inserirComputador(Computador computadores[], 
+    int& quantidade, const Computador& computador) {
+    if (quantidade >= CAPACIDADE) {
+        return false;
+    }
+
+    computadores[quantidade] = computador;
+    quantidade++;
+
+    return true;
+}
 
 int main() {
+    Computador computadores[CAPACIDADE];
+    int quantidade = 0;
+
+    Computador computador = cadastrarComputador();
+
+    if (inserirComputador(computadores, quantidade, computador)) {
+        cout << "\nComputador inserido com sucesso.\n";
+    } else {
+        cout << "\nNao foi possivel inserir o computador.\n";
+    }
+
+    computadores[0].printDados();
+
     return 0;
 }
